e7050c44d24b860ba48d632b86cfa46c53ff12c3
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-07-25 12:22:02 +0430

    add Home and About pages

---
+++ src/components/about/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div className="jumbotron">
+      <h1 className="m-auto">About page</h1>
+      <p>This app uses React, React-Router and Redux.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
