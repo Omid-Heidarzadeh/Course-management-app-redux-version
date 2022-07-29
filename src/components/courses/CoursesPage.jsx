ef6e3216308b8ba77417e6d14012bb725074aad2
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-07-29 12:49:43 +0430

    connect courses page to redux store

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as courseActions from '../../Redux/actions/courseActions';
+import { bindActionCreators } from 'redux';
 
-function CoursesPage() {
-  const [course, setCourse] = useState({ title: '' });
+function CoursesPage(props) {
+  const initialCourse = { title: '' };
+  const [course, setCourse] = useState(initialCourse);
 
   function handleChange(event) {
     const value = event.target.value;
@@ -13,12 +18,27 @@ function CoursesPage() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(JSON.stringify(course, null, 2));
+    props.actions.createCourse(course);
+    setCourse(initialCourse);
   }
 
   return (
     <main>
       <h1>Courses</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.courses.map((course, index) => (
+            <tr key={index}>
+              <td>{course.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <form onSubmit={handleSubmit} className="form">
         <div className="form-group">
           <label htmlFor="title">Title</label>
@@ -39,4 +59,15 @@ function CoursesPage() {
   );
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({ courses: state.courses });
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(courseActions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
