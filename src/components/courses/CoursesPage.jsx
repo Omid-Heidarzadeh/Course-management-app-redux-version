339379548e3ad300795c0f8be97bd90b39ae63d6
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-07-30 12:09:44 +0430

    add CourseList Component

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -3,58 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import * as courseActions from '../../Redux/actions/courseActions';
 import { bindActionCreators } from 'redux';
+import CourseList from './CourseList.jsx';
 
 function CoursesPage(props) {
-  const initialCourse = { title: '' };
-  const [course, setCourse] = useState(initialCourse);
-
-  function handleChange(event) {
-    const value = event.target.value;
-    const name = event.target.name;
-    setCourse((course) => {
-      return { ...course, [name]: value };
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    props.actions.createCourse(course);
-    setCourse(initialCourse);
-  }
 
   return (
     <main>
       <h1>Courses</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.courses.map((course, index) => (
-            <tr key={index}>
-              <td>{course.title}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <form onSubmit={handleSubmit} className="form">
-        <div className="form-group">
-          <label htmlFor="title">Title</label>
-          <input
-            id="title"
-            name="title"
-            className="form-control"
-            type="text"
-            value={course.title}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Save
-        </button>
-      </form>
+      <CourseList courses={courses} />
     </main>
   );
 }
