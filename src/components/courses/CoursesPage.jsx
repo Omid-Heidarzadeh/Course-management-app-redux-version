b5494b476435a60314040af6e83d2f045be22cea
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-07-30 15:24:49 +0430

    connect CoursesPage to authors store

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import * as courseActions from '../../Redux/actions/courseActions';
+import * as authorActions from '../../Redux/actions/authorActions';
 import { bindActionCreators } from 'redux';
 
 function CoursesPage(props) {
+  const { actions, authors, courses } = props;
   const initialCourse = { title: '' };
   const [course, setCourse] = useState(initialCourse);
 
@@ -21,6 +23,19 @@ function CoursesPage(props) {
     props.actions.createCourse(course);
     setCourse(initialCourse);
   }
+
+  useEffect(() => {
+    if (!courses.length)
+      actions.loadCourses().catch((err) => {
+        console.error(err);
+        alert('Error loading courses, refresh the page to try again.');
+      });
+    if (!authors.length)
+      actions.loadAuthors().catch((err) => {
+        console.error(err);
+        alert('Error loading authors, refresh the page to try again.');
+      });
+  }, []);
 
   return (
     <main>
@@ -61,14 +76,27 @@ function CoursesPage(props) {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = (state) => ({ courses: state.courses });
+const mapStateToProps = (state) => ({
+  courses:
+    state.authors.length === 0
+      ? []
+      : state.courses.map((course) => ({
+          ...course,
+          authorName: state.authors.find(
+            (author) => author.id === course.authorId
+          ).name,
+        })),
+  authors: state.authors,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   actions: {
     loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+    loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
   },
 });
 
