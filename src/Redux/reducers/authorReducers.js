f475084deebc07aa267eabc09881370aad8abaaf
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-07-30 14:22:57 +0430

    set initial state of course and author stores

--- src/Redux/reducers/authorReducers.js
+++ src/Redux/reducers/authorReducers.js
@@ -1,6 +1,7 @@
 import * as types from '../actions/actionTypes';
+import initalState from './initialState';
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initalState.authors, action) {
   switch (action.type) {
     case types.LOAD_AUTHORS_SUCCESS: {
       return action.authors;
